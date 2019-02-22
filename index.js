f09e1013cb877be3f5bb13d4687afb7e900df69c
Tom Sullivan <tom@msbit.com.au>
2019-02-23 09:58:26 +1100

    Initial

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+  const head = document.getElementsByTagName('head')[0];
+  const script = document.createElement('script');
+  script.async = true;
+  script.defer = true;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=initMap`;
+  script.type = 'text/javascript';
+  head.appendChild(script);
+});
+
+function initMap() {
+}
