cd344a197c75862e02692c4b8f751ae9de428597
Tom Sullivan <tom@msbit.com.au>
2021-04-25 23:27:31 +1000

    Build JS URL query string

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ window.addEventListener('load', () => {
   const head = document.getElementsByTagName('head')[0];
   const script = document.createElement('script');
 
+  const url = new URL('https://maps.googleapis.com/maps/api/js');
+  url.searchParams.set('callback', 'initMap');
+  url.searchParams.set('key', GOOGLE_MAPS_API_KEY);
+  url.searchParams.set('libraries', 'geometry');
+
   script.async = true;
   script.defer = true;
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&callback=initMap&libraries=geometry`;
+  script.src = url.toString();
   script.type = 'text/javascript';
 
   head.appendChild(script);
